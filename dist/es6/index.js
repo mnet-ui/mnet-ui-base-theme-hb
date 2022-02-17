caa079b6a7158ed572267f3d9da82a5d57bb716b
saumik.s <saumik.s@media.net>
2022-02-17 11:15:16 +0530

    [global -> focus] Color set transparent

--- dist/es6/index.js
+++ dist/es6/index.js
@@ -291,7 +291,7 @@ export var generate = function generate(baseSpacing, scale) {
         // shadow or outline are required for accessibility
         border: {
           // remove to only have shadow
-          color: 'focus'
+          color: 'transparent'
         },
         // outline: { color: undefined, size: undefined },
         shadow: {
